68c014c0162847ceb55cadace37381cb56f7cf59
Jason A. Grafft <jason.grafft@relational.ai>
2023-03-25 12:37:33 -0500

    Updated SQL Schema

--- fswebcam/scd30/scd30_fswebcam_leaven_monitor.sql
+++ fswebcam/scd30/scd30_fswebcam_leaven_monitor.sql
@@ -12,6 +12,17 @@ CREATE TABLE "rpi"."sample_times" (
   "ts" TIMESTAMP WITH TIME ZONE UNIQUE NOT NULL
 );
 
+CREATE TABLE "rpi"."sessions" (
+  "session_id" INTEGER PRIMARY KEY,
+  "start_ts" INTEGER NOT NULL,
+  "end_ts" INTEGER NOT NULL
+);
+
+CREATE TABLE "rpi"."session_ids" (
+  "id" INTEGER PRIMARY KEY,
+  "label" TEXT UNIQUE NOT NULL
+);
+
 CREATE TABLE "scd30"."carbon_dioxide" (
   "ts_id" INTEGER PRIMARY KEY,
   "CO2" NUMERIC(16,9) NOT NULL
@@ -27,6 +38,8 @@ CREATE TABLE "scd30"."relative_humidity" (
   "RH" NUMERIC(16,9) NOT NULL
 );
 
+CREATE UNIQUE INDEX ON "rpi"."sessions" ("session_id", "start_ts", "end_ts");
+
 CREATE UNIQUE INDEX ON "scd30"."carbon_dioxide" ("ts_id", "CO2");
 
 CREATE UNIQUE INDEX ON "scd30"."degrees_celsius" ("ts_id", "C");
@@ -37,6 +50,10 @@ COMMENT ON TABLE "rpi"."images" IS 'fswebcam utlity';
 
 COMMENT ON TABLE "rpi"."sample_times" IS 'Raspberry Pi clock';
 
+COMMENT ON TABLE "rpi"."sessions" IS 'Timestamp range for each session';
+
+COMMENT ON TABLE "rpi"."session_ids" IS 'Integer ids and text labels for sessions';
+
 COMMENT ON TABLE "scd30"."carbon_dioxide" IS 'Adafruit SCD30';
 
 COMMENT ON TABLE "scd30"."degrees_celsius" IS 'Adafruit SCD30';
@@ -44,6 +61,12 @@ COMMENT ON TABLE "scd30"."degrees_celsius" IS 'Adafruit SCD30';
 COMMENT ON TABLE "scd30"."relative_humidity" IS 'Adafruit SCD30';
 
 ALTER TABLE "rpi"."images" ADD FOREIGN KEY ("ts_id") REFERENCES "rpi"."sample_times" ("id");
+
+ALTER TABLE "rpi"."sessions" ADD FOREIGN KEY ("session_id") REFERENCES "rpi"."session_ids" ("id");
+
+ALTER TABLE "rpi"."sessions" ADD FOREIGN KEY ("start_ts") REFERENCES "rpi"."sample_times" ("id");
+
+ALTER TABLE "rpi"."sessions" ADD FOREIGN KEY ("end_ts") REFERENCES "rpi"."sample_times" ("id");
 
 ALTER TABLE "scd30"."carbon_dioxide" ADD FOREIGN KEY ("ts_id") REFERENCES "rpi"."sample_times" ("id");
 
